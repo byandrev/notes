bcb9eaa212c874e850eeaa1beb80602e9abb21ab
Andersson Cardenas <anderssonccg@gmail.com>
2023-09-18 10:26:38 -0400

    Testing bugs

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from fastapi.staticfiles import StaticFiles
-
+from starlette.responses import RedirectResponse
 from core.settings import settings
 from routers.base import router
+from app import models
+from app.schemas.user import User
+from app.database import SessionLocal, engine
 
+models.Base.metadata.create_all(bind = engine)
 
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+  
 def start_application():
   application: FastAPI = FastAPI(title=settings["PROJECT_NAME"],
                                  version=settings["PROJECT_VERSION"])
@@ -16,6 +28,13 @@ def start_application():
 
 app = start_application()
 
+@app.post("/user/")
+async def create_user(db: Session = Depends(get_db)):
+  user = User()
+  db.add(user)
+  db.commit()
+  response = RedirectResponse('/', status_code = 303)
+  return response
 
 if __name__ == "__main__":
   IS_RELOAD = settings["ENVIRONMENT"] == "development"
