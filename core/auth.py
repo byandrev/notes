6316e34a558a85ea9e4a7ab179c0c89329462158
Andres Parra <byandrev@gmail.com>
2023-10-05 21:18:59 -0500

    create functions to auth user and get user

--- core/auth.py
+++ core/auth.py
@@ -1,11 +1,11 @@
 from typing import Union
-from fastapi import Depends, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 
 from core.hashing import Hasher
 from core.security import decode_token
-from schemas.user import UserInDB, User
+from schemas.user import User
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -17,25 +17,29 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     username: str = payload.get("sub")
 
     if username is None:
-      raise Exception("Username not found")
+      return None
   except JWTError:
-    raise Exception("JWT Error")
+    return None
 
   # user: User = User(**repository.get_user(username))
+  user: User = None
 
   if user is None:
-    raise credentials_exception
+    return None
 
   return user
 
 
 def authenticate_user(username: str, password: str) -> Union[User, bool]:
-  user = repository.get_user(username)
+  # user = repository.get_user(username)
+  print(username)
+  user: User = None
 
-  if not user:
+  if user is None:
     return False
 
-  user_db: UserInDB = UserInDB(**user)
+  # user_db: UserInDB = UserInDB(**user)
+  user_db = None
 
   if not Hasher.verify_password(password, user_db.hashed_password):
     return False
--- db/serializers/user_serializer.py
+++
@@ -1,7 +0,0 @@
-def user_entity(user) -> dict:
-  return {
-    "id": str(user["_id"]),
-    "username": user["username"],
-    "email": user["email"],
-    "hashed_password": user["hashed_password"]
-  }
